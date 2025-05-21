8b43102b780ef76cfc6bd8923a8ff35ae50e1516
Akshay Patidar <akshaypatidar1999@gmail.com>
2025-05-21 14:49:17 +0530

    docs: add missing java docs
    Signed-off-by: Akshay Patidar <akshaypatidar1999@gmail.com>

--- src/main/java/com/dream11/grpc/util/AnnotationUtil.java
+++ src/main/java/com/dream11/grpc/util/AnnotationUtil.java
@@ -6,8 +6,19 @@ import java.util.List;
 import lombok.experimental.UtilityClass;
 import org.reflections.Reflections;
 
+/**
+ * Utility class for working with Java annotations. This class provides methods to scan and process
+ * classes with specific annotations.
+ */
 @UtilityClass
 public class AnnotationUtil {
+  /**
+   * Scans a specified package and returns all classes that are annotated with the given annotation.
+   *
+   * @param packageName The name of the package to scan for annotated classes
+   * @param annotation The annotation class to look for
+   * @return A list of classes that are annotated with the specified annotation
+   */
   public List<Class<?>> getClassesWithAnnotation(
       String packageName, Class<? extends Annotation> annotation) {
     return new ArrayList<>(new Reflections(packageName).getTypesAnnotatedWith(annotation));
